21ec5e6f5fb731273aca63a88ccc102e25c24374
smsguy927 <smsguy927@gmail.com>
2021-05-18 13:02:44 -0500

    commit tree 36b1174c02e1fa561f8044bb37aad3e15e8e286d

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(arr, name) {
+  name = name.toLowerCase();
+  return arr.filter(x => x.toLowerCase() === name)
+}
+
+function fuzzyMatch(arr, chars) {
+  return arr.filter(x => x.slice(0, chars.length) === chars)
+}
+
+function matchName (arr, firstName) {
+  return arr.filter(x => x.name === firstName)
+}
